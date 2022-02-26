6d910f532c33f66819e494bc95a05b1f6eae8658
oguzhandeniz <oguzhandeniz2512@gmail.com>
2022-02-26 17:18:34 +0300

    Create formHomework.php

---
+++ forms/formHomework/formHomework.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework</title>
+</head>
+<body>
+
+<form action="post.php" method="post">
+    <input type="number" name="num" placeholder="Number"><br><br>
+    <button type="submit">Send</button>
+</form>
+
+</body>
+</html>
