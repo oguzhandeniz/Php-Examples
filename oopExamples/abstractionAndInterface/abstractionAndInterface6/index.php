eee71d27c4a8b7f1f726426f3d6e27882f0d20e1
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-06-14 04:12:38 +0300

    Create index.php

---
+++ oopExamples/abstractionAndInterface/abstractionAndInterface6/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+        interface Deneme
+        {
+            public function Tanim($ParametreIcerigi);
+            // Hata kodu döndürür, çünkü soyut arayüz sınıfı içerisinde bulunan metodlar muhakkak soyut arayüz sınıfı tanımlanan sınıflarda da bulunmalıdır.
+
+        }
+
+        class TestBir implements Deneme
+        {
+        }
+
+        class TestIki implements Deneme
+        {
+        }
+
+
+    ?>
+</body>
+
+</html>
