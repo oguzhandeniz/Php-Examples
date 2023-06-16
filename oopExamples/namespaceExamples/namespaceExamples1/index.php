8c8e0c62f938ce2c78b9445b6cf6483b4913f178
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-06-17 00:39:52 +0300

    Create index.php

---
+++ oopExamples/namespaceExamples/namespaceExamples1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        /*
+        namespace	:	Sınıflar için özel isim alanları / küme tanımlamak için kullanılır.
+        */
+
+        class Example
+        { // Hata kodu döndürür, çünkü Example sınıfı birden fazla kez kullanılmıştır.
+
+        }
+
+        class Example
+        {
+        }
+
+    ?>
+</body>
+
+</html>
