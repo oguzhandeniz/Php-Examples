0751db0815cda3e4fb41c3ffb16c406448d45241
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-06-19 03:01:35 +0300

    Create index.php

---
+++ oopExamples/namespaceExamples/namespaceExamples4/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once("Classes/HalimCanOcakli.php");
+require_once("Classes/BatinBerkinTopaloglu.php");
+require_once("Classes/OguzhanDeniz.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        $Bir    =    new Halim\Deneme;
+        echo $Bir->Harf . "<br />";
+
+        $Iki    =    new Oguzhan\Deneme;
+        echo $Iki->Harf . "<br />";
+
+        $Uc        =    new Berkin\Deneme;
+        echo $Uc->Harf . "<br />";
+    ?>
+</body>
+
+</html>
