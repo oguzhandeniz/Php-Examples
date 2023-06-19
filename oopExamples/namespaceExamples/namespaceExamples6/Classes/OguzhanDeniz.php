ef2579c0f8353fdabda6fbf5ef0fd500a42e5249
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-06-19 03:01:51 +0300

    Create OguzhanDeniz.php

---
+++ oopExamples/namespaceExamples/namespaceExamples6/Classes/OguzhanDeniz.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Uyeler\Oguzhan\Islemler\Kaydet;
+
+class Deneme {
+    public $Harf	=	"O";
+}
+
+namespace Odemeler\Oguzhan;
+
+class Test {
+
+	public $Rakam	=	"3";
+	
+}
+
+
+?>
