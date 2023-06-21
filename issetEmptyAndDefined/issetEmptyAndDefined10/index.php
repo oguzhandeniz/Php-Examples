1e3069014573e1fe4eaf1426cabb6628ef92d180
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-06-21 17:25:06 +0300

    Create index.php

---
+++ issetEmptyAndDefined/issetEmptyAndDefined10/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        $isim        =    "Oğuzhan Deniz";
+
+        $control    =    empty($isim);
+
+        if ($control == true) {
+            echo 'isim adında değişken bulunmamaktadır.';
+        } else {
+            echo 'isim adında değişken bulunmaktadır.';
+        }
+    ?>
+</body>
+
+</html>
