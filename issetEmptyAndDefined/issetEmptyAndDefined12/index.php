7b8d4772d44e0120b44784a00eedc147470bd64b
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-06-21 17:25:11 +0300

    Create index.php

---
+++ issetEmptyAndDefined/issetEmptyAndDefined12/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+       $values	=	array("Esya" => "Masa", "Renk" => "Mavi", "Vasita" => "Uçak");
+	
+       $control	=	empty($values["Renk"]);
+       
+       if($control==true){
+           echo 'values adındaki dizi içerisinde Renk anahtarı bulunmamaktadır.';
+       }else{
+           echo 'values adındaki dizi içerisinde Renk anahtarı bulunmaktadır.';
+       }
+    ?>
+</body>
+
+</html>
