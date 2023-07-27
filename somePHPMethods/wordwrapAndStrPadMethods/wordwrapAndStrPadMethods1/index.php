330b52627093984e8109e2d7a3740d52ab5a3c2b
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-07-27 07:34:59 +0300

    wordwrapAndStrPadMethods

---
+++ somePHPMethods/wordwrapAndStrPadMethods/wordwrapAndStrPadMethods1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+        /*
+        wordwrap()	:	Belirtilecek olan içeriğe, belirtilecek olan karakter sıra numarası aralıklarına göre, belirtilecek olan değer / değerleri ekleyerek, eklenmiş olan değeri geriye döndürür.
+        str_pad()	:	Belirtilecek olan içeriğin, belirtilecek olan değer / değerler doğrultusunda, başına, sonuna veya hem başına hemde sonuna değer ekleyerek, eklenmiş olan değeri geriye döndürür.
+            STR_PAD_LEFT	:	Belirtilecek olan içeriğin başına / soluna değer eklemek için kullanılır.
+            STR_PAD_RIGHT	:	Belirtilecek olan içeriğin sonuna / sağına değer eklemek için kullanılır.
+            STR_PAD_BOTH	:	Belirtilecek olan içeriğin hem başına / soluna, hemde sonuna / sağına değer eklemek için kullanılır.
+        */
+
+        $text        =    "Back-End Developer Oğuzhan DENİZ";
+        $method        =    wordwrap($text, 8, "<br />");
+
+        echo $method;
+
+    ?>
+</body>
+
+</html>
---
+++ somePHPMethods/wordwrapAndStrPadMethods/wordwrapAndStrPadMethods5/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+        /*
+        wordwrap()	:	Belirtilecek olan içeriğe, belirtilecek olan karakter sıra numarası aralıklarına göre, belirtilecek olan değer / değerleri ekleyerek, eklenmiş olan değeri geriye döndürür.
+        str_pad()	:	Belirtilecek olan içeriğin, belirtilecek olan değer / değerler doğrultusunda, başına, sonuna veya hem başına hemde sonuna değer ekleyerek, eklenmiş olan değeri geriye döndürür.
+            STR_PAD_LEFT	:	Belirtilecek olan içeriğin başına / soluna değer eklemek için kullanılır.
+            STR_PAD_RIGHT	:	Belirtilecek olan içeriğin sonuna / sağına değer eklemek için kullanılır.
+            STR_PAD_BOTH	:	Belirtilecek olan içeriğin hem başına / soluna, hemde sonuna / sağına değer eklemek için kullanılır.
+        */
+
+        
+        $text		=	"Karabük Üniversitesi";
+	
+        echo $text . "<br />";
+        
+        $method		=	str_pad($text, 30, "2023", STR_PAD_BOTH);
+        
+        echo $method;
+	
+    ?>
+</body>
+
+</html>
