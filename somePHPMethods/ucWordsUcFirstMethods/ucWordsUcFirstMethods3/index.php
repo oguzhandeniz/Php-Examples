13508e54d6931a64ee6638f0a8db289fb4203de0
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-08-09 12:06:36 +0300

    ucWordsAndUcFirstMethods

---
+++ somePHPMethods/ucWordsUcFirstMethods/ucWordsUcFirstMethods3/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        /*
+        ucwords()	:	Belirtilecek olan içerikte bulunan tüm kelimelerin ilk harflerini büyük harf olacak şekilde değiştirerek, değiştirilmiş olan değeri geriye döndürür.
+        ucfirst()	:	Belirtilecek olan içeriğin ilk harfini büyük harf olacak şekilde değiştirerek, değiştirilmiş olan değeri geriye döndürür.
+        lcfirst()	:	Belirtilecek olan içeriğin ilk harfini küçük harf olacak şekilde değiştirerek, değiştirilmiş olan değeri geriye döndürür.
+        */
+
+        $value            =    "FENERBAHÇE - OĞUZHAN DENİZ - KARABÜK ÜNİVERSİTESİ - İSTANBUL";
+
+        echo $value . "<br />";
+	
+        $text			=	lcfirst($value);
+        
+        echo $text;
+
+    ?>
+</body>
+
+</html>
