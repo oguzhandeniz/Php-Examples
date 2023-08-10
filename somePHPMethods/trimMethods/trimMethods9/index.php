bb5bf22f50272353e07b86adcbd3250b7618bd22
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-08-10 13:19:41 +0300

    trimMethod

---
+++ somePHPMethods/trimMethods/trimMethods9/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+     /*
+        trim()		:	Belirtilecek olan içeriğin, varsa başındaki ve sonundaki tüm boşlukları, sekmeleri ve yeni satır karakterlerini silerek, oluşturduğu değeri geriye döndürür. Ayrıca belirtilecek olan içeriğin, varsa başındaki ve sonundaki, belirtilecek olan karakterleride silerek, oluşturduğu değeri geriye döndürür.
+        ltrim()		:	Belirtilecek olan içeriğin, varsa başındaki / solundaki tüm boşlukları, sekmeleri ve yeni satır karakterlerini silerek, oluşturduğu değeri geriye döndürür. Ayrıca belirtilecek olan içeriğin, varsa başındaki / solundaki, belirtilecek olan karakterleride silerek, oluşturduğu değeri geriye döndürür.
+        rtrim()		:	Belirtilecek olan içeriğin, varsa sonundaki / sağındaki tüm boşlukları, sekmeleri ve yeni satır karakterlerini silerek, oluşturduğu değeri geriye döndürür. Ayrıca belirtilecek olan içeriğin, varsa sonundaki / sağındaki, belirtilecek olan karakterleride silerek, oluşturduğu değeri geriye döndürür.
+        chop()		:	Belirtilecek olan içeriğin, varsa sonundaki / sağındaki tüm boşlukları, sekmeleri ve yeni satır karakterlerini silerek, oluşturduğu değeri geriye döndürür. Ayrıca belirtilecek olan içeriğin, varsa sonundaki / sağındaki, belirtilecek olan karakterleride silerek, oluşturduğu değeri geriye döndürür.
+        */
+
+       
+        $value        =    "     İstanbul 34     ";
+
+        echo $value . "<br />";
+
+        echo chop($value);
+
+    ?>
+</body>
+
+</html>
