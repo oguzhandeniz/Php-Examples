02eaf6ab8b16483b1c5a0c3d5e8a6fcd9ebbcbdd
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-09-21 17:24:14 +0300

    base64EncodeDecodeMethods

---
+++ somePHPConvertingMethods/base64EncodeDecodeMethods/base64EncodeDecodeMethods2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    /*
+	base64_encode()		:	Belirtilecek olan içeriğin base64 (8 Bit'lik verilerin 64 Bit'lik verilere çevrilmesi) algoritması kodlamasını bularak, bulduğu değeri geriye döndürür.
+	base64_decode()		:	Belirtilecek olan ve base64 (8 Bit'lik verilerin 64 Bit'lik verilere çevrilmesi) algoritması kullanılarak kodlanmış olan içeriği geri çözerek, çözdüğü değeri geriye döndürür.
+	*/
+
+    $text		=	"T8SfdXpoYW4gRGVuaXogLSDEsHN0YW5idWw=";
+	echo $text . "<br />";
+	$method	=	base64_decode($text);
+	echo $method;
+
+    ?>
+</body>
+
+</html>
