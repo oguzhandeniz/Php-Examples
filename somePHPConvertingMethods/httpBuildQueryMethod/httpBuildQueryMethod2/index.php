3b5a0c0d667feaeb9d85e62767e4c003974301fe
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-09-21 17:24:36 +0300

    httpBuildQueryMethods

---
+++ somePHPConvertingMethods/httpBuildQueryMethod/httpBuildQueryMethod2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+        /*
+        http_build_query()	:	Belirtilecek olan dizi içerisindeki tüm anahtar ve değerleri bularak, bulduğu değerleri URL kodlamalı bir sorgu dizisine dönüştürerek, dönüştürdüğü değeri geriye döndürür.
+        */
+
+        $values	=	array("Web Programlama", "Görsel Eğitim Seti", "PHP");
+        echo "<pre>";
+        print_r($values);
+        echo "</pre>";
+        
+        $sonuc		=	http_build_query($values);
+        echo $sonuc;
+
+
+    ?>
+</body>
+
+</html>
---
+++ somePHPConvertingMethods/httpBuildQueryMethod/httpBuildQueryMethod4/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+        /*
+        http_build_query()	:	Belirtilecek olan dizi içerisindeki tüm anahtar ve değerleri bularak, bulduğu değerleri URL kodlamalı bir sorgu dizisine dönüştürerek, dönüştürdüğü değeri geriye döndürür.
+        */
+
+        $values	=	array("EgitimTuru" => "Web Programlama", "EgitimSekli" => "Görsel Eğitim Seti", "KodlamaDili" => "PHP");
+        echo "<pre>";
+        print_r($values);
+        echo "</pre>";
+        
+        $sonuc		=	http_build_query($values, "Ozel_");
+        echo $sonuc;
+
+
+    ?>
+</body>
+
+</html>
