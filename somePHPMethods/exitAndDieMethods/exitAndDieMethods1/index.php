536a3448bfd28128290c6366bdb01a4d54b1db2e
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-10-21 17:58:07 +0300

    exitAndDieMethods

---
+++ somePHPMethods/exitAndDieMethods/exitAndDieMethods1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        /*
+        exit()	:	Kullanıldığı satırdan sonraki tüm kodlamaların çalışmasını iptal etmek / sonlandırmak için kullanılır. Ayrıca istenirse ek olarak belirtilecek olan değeri geriye döndürür.
+        die()	:	Kullanıldığı satırdan sonraki tüm kodlamaların çalışmasını iptal etmek / sonlandırmak için kullanılır. Ayrıca istenirse ek olarak belirtilecek olan değeri geriye döndürür.
+        */
+
+        echo "Hello WORLD !!!<br />";
+
+        exit();
+
+        echo "Oğuzhan Deniz";
+    ?>
+</body>
+
+</html>
