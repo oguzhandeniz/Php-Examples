95b81976e2e91325c00633e52280f29f39971629
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-10-23 10:24:26 +0300

    highlightStringMethod

---
+++ somePHPMethods/highlightStringMethod/highlightStringMethod3/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+       /*
+        highlight_string()		:	Belirtilecek olan herhangi bir PHP kodlamasını, PHP'nin yerleşik söz dizimi vurgulayıcısında tanımlanmış olan rekleri kullanarak, vurgulu bir halde ekran çıktılanmasını sağlamak için kullanılır.
+        */
+
+        highlight_string('<?php
+        phpinfo();
+?>');
+
+
+    ?>
+</body>
+
+</html>
