0538784c3056b3d63d6fef298fc6114ea815eea1
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-10-25 14:55:17 +0300

    slashesMethods

---
+++ somePHPMethods/slashesMethods/slashesMethods2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        /*
+        addslashes()	:	Belirtilecek olan içerikte bulunan ' (tek tırnak) ve " (çift tırnak) işaretlerinin önüne \ (ters slash) yani kaçırma operatörü ekler ve sade bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        stripslashes()	:	Belirtilecek olan içerikte bulunan ve önüne \ (ters slash) yani kaçırma operatörü eklenmiş olan tüm karakterlerin önünde bulunan \ (ters slash) yani kaçırma operatörlerini kaldırır ve aktif bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        addcslashes()	:	Belirtilecek olan içerikte bulunan, belirtilecek olan özel karakterlerin önüne \ (ters slash) yani kaçırma operatörü ekler ve sade bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        stripcslashes()	:	Belirtilecek olan içerikte bulunan ve önüne \ (ters slash) yani kaçırma operatörü eklenmiş olan tüm közel arakterlerin önünde bulunan \ (ters slash) yani kaçırma operatörlerini kaldırır ve aktif bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        quotemeta()		:	Belirtilecek olan içerikte bulunan, özel karakterlerin önüne \ (ters slash) yani kaçırma operatörü ekler ve sade bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür. Karakterler . $ \ ? + * ^ [ ] ( )
+        */
+       
+        $text        =    "Oğuzhan Deniz - A'dan Z'ye PHP Örnek Kodları";
+        echo $text . "<br /><br />";
+
+        $newText    =    addslashes($text);
+        echo $newText . "<br /><br />";
+
+        $lastText    =    stripslashes($newText);
+        echo $lastText;
+
+    ?>
+</body>
+
+</html>
---
+++ somePHPMethods/slashesMethods/slashesMethods7/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+        /*
+        addslashes()	:	Belirtilecek olan içerikte bulunan ' (tek tırnak) ve " (çift tırnak) işaretlerinin önüne \ (ters slash) yani kaçırma operatörü ekler ve sade bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        stripslashes()	:	Belirtilecek olan içerikte bulunan ve önüne \ (ters slash) yani kaçırma operatörü eklenmiş olan tüm karakterlerin önünde bulunan \ (ters slash) yani kaçırma operatörlerini kaldırır ve aktif bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        addcslashes()	:	Belirtilecek olan içerikte bulunan, belirtilecek olan özel karakterlerin önüne \ (ters slash) yani kaçırma operatörü ekler ve sade bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        stripcslashes()	:	Belirtilecek olan içerikte bulunan ve önüne \ (ters slash) yani kaçırma operatörü eklenmiş olan tüm közel arakterlerin önünde bulunan \ (ters slash) yani kaçırma operatörlerini kaldırır ve aktif bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür.
+        quotemeta()		:	Belirtilecek olan içerikte bulunan, özel karakterlerin önüne \ (ters slash) yani kaçırma operatörü ekler ve sade bir metin içeriği oluşturarak, oluşturduğu değeri geriye döndürür. Karakterler . $ \ ? + * ^ [ ] ( )
+        */
+
+        $text        =    "Oğuzhan Deniz - A'dan Z'ye PHP Örnek Kodları";
+        echo $text . "<br /><br />";
+
+        $newText    =    addcslashes($text, "a..z");
+        echo $newText . "<br /><br />";
+
+        $lastText    =    stripcslashes($text);
+        echo $lastText;
+
+    ?>
+</body>
+
+</html>
